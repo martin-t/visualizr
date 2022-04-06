649bdca90c23baffa90148adde63e7f97afb5bab
Martin Taibr <taibr.martin@gmail.com>
2022-04-06 23:28:25 +0200

    Silence a warn

--- bindingsr/src/lib.rs
+++ bindingsr/src/lib.rs
@@ -2,6 +2,7 @@
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
+#[allow(deref_nullptr)] // https://github.com/rust-lang/rust-bindgen/issues/1651
 #[allow(clippy::all)]
 mod bindings;
 
